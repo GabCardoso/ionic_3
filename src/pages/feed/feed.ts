ed0b3d2c0d0ea1b13e36bbd27aeb79f9b89bde6b
Gabriela Cardoso <gabrielacfontes.gc@gmail.com>
2019-12-02 20:46:59 -0300

    first commit

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+
+  public nomeUsuario: String = 'Gab Cardoso'
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FeedPage');
+  }
+
+}
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+import { FeedPage } from '../feed/feed';
+import { IntroPage } from '../intro/intro';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  // tab2Root = AboutPage;
+  // tab3Root = ContactPage;
+  tab4Root = FeedPage;
+  tab5Root = IntroPage;
+
+  constructor() {
+
+  }
+}
